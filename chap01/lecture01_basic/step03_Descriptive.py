ceb91015673b37fce7a26360ca347f8e24af0501
Minjeong Kim <ppz7227@naver.com>
2024-04-08 09:47:18 +0900

    Create step03_Descriptive.py

---
+++ chap01/lecture01_basic/step03_Descriptive.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+"""
+step03_Descriptive.py
+
+1. DataFrame의 요약통계량 
+2. 상관계수
+"""
+
+import pandas as pd 
+
+path = r'C:\ITWILL\4_Python_ML\data'
+
+product = pd.read_csv(path + '/product.csv')
+
+
+# DataFrame 정보 보기 
+product.info()
+
+
+# 앞부분/뒷부분 관측치 5개 보기 
+product.head()
+product.tail()
+
+# 1. DataFrame의 요약통계량 
+summ = product.describe()
+print(summ)
+
+# 행/열 통계
+product.shape # (264, 3)
+product.sum(axis = 0) # 행축  
+product.sum(axis = 1) # 열축 
+
+# 산포도 : 분산, 표준편차 
+product.var() # axis = 0
+product.std() # axis = 0
+
+# 빈도수 : 집단변수 
+product['a'].value_counts()
+
+
+# 유일값 
+product['b'].unique()
+
+# 2. 상관관계 
+cor = product.corr()
+print(cor) # 상관계수 행렬 
+
+
